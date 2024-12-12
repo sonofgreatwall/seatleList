ef3984b517dbaeef9c6ae2c1e25f651e92cd046b
Vitalii Dendura <unclescar0813@gmail.com>
2024-12-12 23:54:08 +0900

    top navbar

--- src/app/ui/Navtab.tsx
+++ src/app/ui/Navtab.tsx
@@ -14,20 +14,19 @@ export default function Navtab() {
   ];
 
   return (
-    <nav className="flex items-center bg-gray-100 rounded-full shadow-md p-1 space-x-2 w-max m-6 navtab">
+    <nav className="flex items-center bg-gray-100 rounded-full shadow-md p-1 space-x-2 w-max navtab mx-auto lg:m-6 ">
       {tabs.map(({ name, url }) => (
         <Link
           key={name}
           href={"/"}
-          className={`px-6 py-2 text-sm font-semibold rounded-full transition-all duration-200 whitespace-nowrap ${
-            activeTab === name
-              ? "bg-black text-white"
-              : "text-black hover:bg-gray-200"
-          }`}
+          className={`px-6 py-2 text-sm font-semibold rounded-full transition-all duration-200 whitespace-nowrap flex ${activeTab === name
+            ? "bg-black text-white"
+            : "text-black hover:bg-gray-200"
+            }`}
           onClick={() => setActiveTab(name)}
         >
-          {name}
-          <Image src={url} width={20} height={20} alt={name} className="" />
+          <span className="hidden lg:block p-2">{name}</span>
+          <Image src={url} width={25} height={25} alt={name} className="" />
         </Link>
       ))}
     </nav>
