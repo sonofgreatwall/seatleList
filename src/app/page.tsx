c6a3f962e01bab6e32b240e3f9a26a6abeb2b385
Vitalii Dendura <unclescar0813@gmail.com>
2024-12-16 05:27:33 +0900

    update map movable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+"use client"; // Mark this file as a client component
+
+import { useState, useRef, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Filter from "./ui/Filter";
 import Results from "./ui/Results";
 import Navtab from "./ui/Navtab";
-import { useState } from "react";
 import BottomRibbon from "./ui/BottomRibbon";
 
 // Dynamically import the MapComponent with SSR disabled
@@ -14,21 +15,45 @@ const MapComponent = dynamic(() => import("./ui/components/MapComponent"), {
 export default function Home() {
   const [toggleMap, setToggleMap] = useState<number>(1);
   const [activeMap, setActiveMap] = useState<number>(0);
-  console.log(activeMap);
-  
+  const mapRef = useRef<HTMLDivElement>(null); 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (mapRef.current && !mapRef.current.contains(event.target as Node)) {
+        setActiveMap(0);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
       <Navtab />
-      <div className={`main ${toggleMap === 1 ? "grid-cols-2" : "grid-cols-1"}`} onClick={() => {setActiveMap(0)}}>
+      <div className={`main ${toggleMap === 1 ? "grid-cols-2" : "grid-cols-1"}`}>
         {toggleMap === 1 && (
-          <div onDoubleClick={() => { setActiveMap(1); }}>
-            <div className={`map ${activeMap == 1? `z-10` : `z-[-30]`} `} >
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            onDoubleClick={(e) => {
+              e.preventDefault();
+              if (activeMap === 0) setActiveMap(1);
+              else setActiveMap(0);
+            }}
+          >
+            <div
+              ref={mapRef} // Attach the ref to the map container
+              className={`map ${
+                activeMap === 1 ? "pointer-events-auto" : "pointer-events-none"
+              }`}
+            >
               <MapComponent />
             </div>
           </div>
         )}
-        {/* sdasa */}
         <div className="max-w-[1100px] mx-auto">
           <div className="filter-results-container">
             <div className="filter">
